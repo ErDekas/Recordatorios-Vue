2e13ce487503852f027b77b40a41e37599dc7bfa
Deka <150124896+ErDekas@users.noreply.github.com>
2025-01-07 08:55:07 +0100

    Update App.vue

--- Recordatorios/src/App.vue
+++ Recordatorios/src/App.vue
@@ -36,7 +36,7 @@
       <div class="footer">
         Desarrollado por "Pablo Linares".<br />
         Código disponible en
-        <a href="https://github.com/ErDekas/Recordatorio" target="_blank">GitHub</a>.
+        <a href="https://github.com/ErDekas/Recordatorios-Vue" target="_blank">GitHub</a>.
       </div>
     </div>
   </div>
